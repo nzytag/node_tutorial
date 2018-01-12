af1f447b5b12ef55263644c7cd3729d2be80207a
Hector <norzagaray@me.com>
2018-01-11 17:10:36 -0800

    added new demo child

--- requestHandlers.js
+++ requestHandlers.js
@@ -8,14 +8,64 @@
 // }
 // exports.start = start;
 // exports.upload = upload;
+//this is another EXAMPLE
+// function start() {
+//   console.log("Request handler 'start' was called.");
+//   return "Hello start";
+// }
+// function upload() {
+//   console.log("Request handler 'upload' was called.");
+//   return "hello upload";
+// }
+// exports.start = start;
+// exports.upload = upload;
+
+
+// this is a better explanation about blocking and non blocking
+//this code below is for when you go to start, it will wait 10 seconds before displaying hello start.
+// function start() {
+//   console.log("request handler 'start' was called.");
+//   function sleep(milliSeconds){
+//     var startTime = new Date().getTime();
+//     while (new Date().getTime() < startTime + milliSeconds);
+//   }
+//   sleep(10000);
+//   return "Hello Start";
+// }
+// function upload(){
+//   console.log("Request handler 'upload' was called.");
+//   return "Hello Upload";
+// }
+// exports.start = start;
+// exports.upload = upload;
+
+// example 8
+
+var exec = require("child_process").exec;
 
 function start() {
   console.log("Request handler 'start' was called.");
-  return "Hello start";
+  var content = "empty";
+  exec(" Is -iah", function (error, stdout, stderr){
+    content = stdout;
+  });
+  return content;
 }
-function upload() {
+function upload(){
   console.log("Request handler 'upload' was called.");
-  return "hello upload";
+  return "Hello upload";
 }
 exports.start = start;
 exports.upload = upload;
+
+
+
+
+
+
+
+
+
+
+
+//foo
